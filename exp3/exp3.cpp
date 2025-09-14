00f5fcc54160c95de35a83f327684748f439b686
23BCS13467-Misal-Saini <misalsaini35@gmail.com>
2025-09-15 01:22:03 +0530

    Create exp3.cpp

---
+++ exp3/exp3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+   map<int,int> freq;
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+        freq[arr[i]]++;
+    }
+     vector<vector<int>> res;
+        for (auto &x : freq) {
+            res.push_back({x.first, x.second});
+        }
+
+    for (auto &pair : res) {
+        cout << "[" << pair[0] << ", " << pair[1] << "] ";
+    }
+    
+
+
+    return 0;
+}
